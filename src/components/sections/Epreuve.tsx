4d78342044e918577ba3430768b47adf299a208f
Meric Vaillant <vaillantmeric@MacBook-Air-de-Meric.local>
2025-04-29 14:25:00 +0200

    Ajout de la grille E4

--- src/components/sections/Epreuve.tsx
+++ src/components/sections/Epreuve.tsx
@@ -69,6 +69,15 @@ const Epreuve: React.FC = () => {
                     <Link className="w-5 h-5 mr-2" />
                     Consulter ma veille
                   </a>
+                  <a
+                    href="/Tableau de synthèse - Epreuve E4 - BTS SIO 2025.pdf" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center px-5 py-2.5 bg-blue-500 text-white font-semibold rounded-full shadow-md hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-500 transition-all"
+                  >
+                    <Link className="w-5 h-5 mr-2" />
+                    Grille E4
+                  </a>
                 </div>
               </div>
             </div>
